3d5d8f88d81a687a75362048200e375b919b8c2a
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-09-02 21:05:09 +0300

    WIP #26
    Completed **Ticket #26: As a Backend Developer, I want to create a basic REST API for Organizations**
    
    Description:
    Implemented a set of CRUD operations to manage organizations with additional criteria support. The ticket included the following functionality:
    
    1. **Get All By Criteria**: Added support for filtering organizations based on criteria such as organizationId, userId, name, displayName, and organizationIdIn.
    
    2. **Get by Id**: Implemented the ability to retrieve an organization by its unique identifier.
    
    3. **Create**: Implemented the creation of new organizations.
    
    4. **Update by Id**: Added the ability to update an existing organization by its identifier.
    
    5. **Delete by Id**: Implemented the deletion of organizations by their unique identifiers.
    
    6. **Return Object**: Ensured that the returned object for these operations contains only the Organization entity without sub-entities, as requested.
    
    This commit addresses Ticket #26 by implementing a set of CRUD operations for the Organization entity. The operations include:
    
    Related Ticket: #26

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/OrganizationService.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/OrganizationService.java
@@ -13,7 +13,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -49,9 +53,54 @@ public class OrganizationService {
         return organizationRepository.findAll(OrganizationPredicates.build(criteria), pageable);
     }
 
+    public Organization getById(UUID id) throws Exception{
+        return organizationRepository.findById(id)
+                .orElseThrow(() -> new GendoxException("ORGANIZATION_NOT_FOUND", "Organization not found with id: " + id, HttpStatus.NOT_FOUND));
+    }
+
     public List<UserOrganization> getUserOrganizations(String userId) {
         return userOrganizationRepository.findByUserId(UUID.fromString(userId));
     }
+
+    public Organization createOrganization(Organization organization) throws Exception{
+
+        if (organization.getId() != null){
+            throw new GendoxException("NEW_ORGANIZATION_ID_IS_NOT_NULL", "Organization id must be null", HttpStatus.BAD_REQUEST);
+        }
+        organization=organizationRepository.save(organization);
+        return organization;
+
+    }
+
+    public Organization updateOrganization(Organization organization) throws Exception{
+        UUID organizationId = organization.getId();
+        Organization existingOrganization = organizationRepository.findById(organizationId)
+                .orElseThrow(() -> new GendoxException("ORGANIZATION_NOT_FOUND", "Organization not found with id: " + organizationId, HttpStatus.NOT_FOUND));
+
+        // Update the properties of the existingOrganization with the values from the updated organization
+        existingOrganization.setName(organization.getName());
+        existingOrganization.setDisplayName(organization.getDisplayName());
+        existingOrganization.setAddress(organization.getAddress());
+        existingOrganization.setPhone(organization.getPhone());
+        existingOrganization.setCreatedAt(organization.getCreatedAt());
+        existingOrganization.setUpdatedAt(Instant.now());
+
+        //save the update organization
+        existingOrganization = organizationRepository.save(existingOrganization);
+
+        return existingOrganization;
+
+    }
+
+   public void deleteOrganization(UUID id) throws Exception{
+        Organization organization = organizationRepository.findById(id).orElse(null);
+
+        if(organization != null){
+            organizationRepository.deleteById(id);
+        }else{
+            throw new GendoxException("ORGANIZATION_NOT_FOUND", "Organization not found with id: " + id, HttpStatus.NOT_FOUND);
+        }
+   }
 
 
 
