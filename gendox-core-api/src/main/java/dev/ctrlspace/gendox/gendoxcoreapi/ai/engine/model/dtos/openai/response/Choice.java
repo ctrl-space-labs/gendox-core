6a1e5d3e4b6dfa2859497c61f08aebfcf32230b4
Chris Sekas <csekas@ctrlspace.dev>
2023-09-14 17:39:51 +0300

    WIP #35 Create AI-Engine Adapter
    - Add Adapter for OpenAI ADA 2
    - Implement Ada 2 Request and Response models
    - Create OpenAI service with mock data
    - Create a controller to test the AiModelService

---
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/ai/engine/model/dtos/openai/response/Choice.java
@@ -0,0 +1,24 @@
+package dev.ctrlspace.gendox.gendoxcoreapi.ai.engine.model.dtos.openai.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Choice implements Serializable {
+    private Integer index;
+    private String text;
+    @JsonProperty("finish_reason")
+    private String finishReason;
+}
+
+
+
+
