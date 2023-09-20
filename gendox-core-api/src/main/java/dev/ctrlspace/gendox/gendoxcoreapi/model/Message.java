1f732236e32a30d8ee01f7372e108abc994dc3c0
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-09-20 11:31:23 +0300

    FIX #35
    Update **Ticket #35: As a Backend Developer I want to create Embeddings Adapter**
    
    - pom.xml
      - add pg vector dependency
    
    - V20230917_123210__Core_Shcema_initial_tables.sql
      - create gendox_core.message table
      - create gendox_core.embedding table
      - create gendox_core.embedding_group table
      - Rename the table message_logs to audit_logs
      - Rename the ai_models table's column name to modelName
    
    - Create Entities
      - AuditLogs
      - Embedding
      - EmbeddingGroup
      - Message
    
    - EmbeddingsController
      - Implement method getEmbeddings
      - Implement method getSectionEmbedding
      - Implement method getProjectEmbeddings
      - Implement method findCloserSections
    
    - EmbeddingService
      - Implement method createEmbedding
      - Implement method createSectionsEmbedding
      - Implement method createProjectEmbeddings
      - Implement method createAuditLogs
      - Implement method createEmbeddingGroup
      - Implement method createMessage
      - Implement method findCloserSections
      - Implement methods getUserId and getProjectId
    
    - DocumentController
      - Implement upload with multi files
    
    - DocumentService
      - Implement getProjectSections method
    
    - Create DTOs and Converters for
      - EmbeddingGroup
      - Message
    
    - Repositories
      - DocumentInstanceSectionRepository
        - Implement Query findByDocumentInstance
        - Implement Query findByEmbeddingIds
      - EmbeddingRepository
        - Implement Query findNearestNeighbors
      - EmbeddingGroupRepository
        - Implement Query findEmbeddingGroupByEmbeddingId
        - Implement Query findSectionIdByEmbeddingId
      - ProjectDocumentRepository
        - Implement Query findDocumentIdsByProjectId
        - Implement Query findDocumentInstancesByDocumentIds
    
    -Unversioned Files
      - pgvector.iml
    
    Related Ticket: #35

---
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/model/Message.java
@@ -0,0 +1,92 @@
+package dev.ctrlspace.gendox.gendoxcoreapi.model;
+
+import jakarta.persistence.*;
+
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "message", schema = "gendox_core", catalog = "postgres")
+public class Message {
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private UUID id;
+    @Basic
+    @Column(name = "value")
+    private String value;
+    @Basic
+    @Column(name = "created_at")
+    private Instant createdAt;
+    @Basic
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+    @Basic
+    @Column(name = "created_by")
+    private UUID createdBy;
+    @Basic
+    @Column(name = "updated_by")
+    private UUID updatedBy;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UUID createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public UUID getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(UUID updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message message)) return false;
+        return Objects.equals(getId(), message.getId()) && Objects.equals(getValue(), message.getValue()) && Objects.equals(getCreatedAt(), message.getCreatedAt()) && Objects.equals(getUpdatedAt(), message.getUpdatedAt()) && Objects.equals(getCreatedBy(), message.getCreatedBy()) && Objects.equals(getUpdatedBy(), message.getUpdatedBy());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getValue(), getCreatedAt(), getUpdatedAt(), getCreatedBy(), getUpdatedBy());
+    }
+}
--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/DocumentInstanceSectionRepository.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/DocumentInstanceSectionRepository.java
@@ -1,12 +1,24 @@
 package dev.ctrlspace.gendox.gendoxcoreapi.repositories;
 
 import dev.ctrlspace.gendox.gendoxcoreapi.model.DocumentInstanceSection;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.ProjectDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface DocumentInstanceSectionRepository extends JpaRepository<DocumentInstanceSection, UUID> , QuerydslPredicateExecutor<DocumentInstanceSection> {
+
+    @Query("SELECT dis FROM DocumentInstanceSection dis WHERE dis.documentInstance.id = :documentInstanceId")
+    public List<DocumentInstanceSection> findByDocumentInstance(UUID documentInstanceId);
+
+    @Query("SELECT dis FROM DocumentInstanceSection dis " +
+            "INNER JOIN EmbeddingGroup eg ON dis.id = eg.sectionId " + // Adjust the join condition as needed
+            "WHERE eg.embeddingId IN :embeddingIds")
+    public List<DocumentInstanceSection> findByEmbeddingIds(Set<UUID> embeddingIds);
 }
