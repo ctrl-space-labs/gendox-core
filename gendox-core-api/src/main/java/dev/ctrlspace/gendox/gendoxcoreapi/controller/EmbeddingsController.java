d16154e66a037a09a76feb00dfe97ef6a74b30ff
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-10-02 22:37:53 +0300

    FIX #65
    Update **Ticket #65: As a backend developer I want for all controllers to add an swagger operation with its description t**
    
    - Create Operation swagger annotations for controllers
     - DocumentController
     - EmbeddingsController
     - OrganizationController
     - ProjectController.java
     - UserController
    
    Related Ticket: #65

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/controller/EmbeddingsController.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/controller/EmbeddingsController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +41,11 @@ public class EmbeddingsController {
     }
 
     @PostMapping("/embeddings")
+    @Operation(summary = "Get embeddings",
+            description = "Retrieve embeddings for a given text input using an AI model. " +
+                    "This endpoint accepts a BotRequest containing the text input and returns an Ada2Response " +
+                    "containing the embeddings for the input text. Additionally, it stores the embeddings in the database " +
+                    "as an Embedding entity with a unique ID.")
     public Ada2Response getEmbeddings(@RequestBody BotRequest botRequest) {
         Ada2Response ada2Response = aiModelService.askEmbedding(botRequest);
         Embedding embedding = new Embedding();
@@ -54,16 +60,29 @@ public class EmbeddingsController {
 
 
     @PostMapping("/embeddings/sections/{sectionId}")
+    @Operation(summary = "Get section embedding",
+            description = "Retrieve the embedding for a specific section in a project based on the provided section ID and project ID. " +
+                    "This endpoint calculates and returns the embedding of the text content within the specified section. " +
+                    "It also associates the calculated embedding with the section in the database for further analysis.")
     public Embedding getSectionEmbedding(@PathVariable UUID sectionId, @RequestParam String projectId) throws GendoxException {
         return embeddingService.runTrainingForSection(sectionId, UUID.fromString(projectId));
     }
 
     @PostMapping("/embeddings/projects/{projectId}")
+    @Operation(summary = "Get project embeddings",
+            description = "Retrieve embeddings for all sections in a project based on the provided project ID. " +
+                    "This endpoint calculates and returns embeddings for all sections within the specified project. " +
+                    "It performs the embedding calculation for each section and associates the results with the respective sections.")
+
     public List<Embedding> getProjectEmbeddings(@PathVariable UUID projectId) throws GendoxException {
         return embeddingService.runTrainingForProject(projectId);
     }
 
     @PostMapping("/messages/semantic-search")
+    @Operation(summary = "Semantic search for closer sections",
+    description = "Search for sections within a project that are semantically closer to a given message. " +
+            "This endpoint calculates the embedding for the input message and retrieves sections from the specified project " +
+            "that have similar semantic representations.")
     public List<DocumentInstanceSection> findCloserSections(@RequestBody Message message,
                                                             @RequestParam String projectId,
                                                             Pageable pageable) throws GendoxException {
@@ -74,10 +93,7 @@ public class EmbeddingsController {
             throw new GendoxException("MAX_PAGE_SIZE_EXCEED", "Page size can't be more than 5", HttpStatus.BAD_REQUEST);
         }
 
-
         message = embeddingService.createMessage(message);
-
-
 
         List<DocumentInstanceSection> instanceSections = new ArrayList<>();
         instanceSections = embeddingService.findClosestSections(message,UUID.fromString(projectId));
@@ -87,6 +103,10 @@ public class EmbeddingsController {
 
 
     @PostMapping("/messages/semantic-completion")
+    @Operation(summary = "Semantic completion of message",
+            description = "Find a message within a project that semantically completes the given input message. " +
+                    "This endpoint calculates the embedding for the input message and searches for a complementary message " +
+                    "in the context of the provided project.")
     public CompletionMessageDTO getCompletionSearch(@RequestBody Message message,
                                                     @RequestParam String projectId,
                                                     Pageable pageable) throws GendoxException {
