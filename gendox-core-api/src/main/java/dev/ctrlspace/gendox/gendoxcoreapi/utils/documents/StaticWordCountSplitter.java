4d9f327e5b67c5006d8074a8e5b043a0198761df
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-10-04 14:27:23 +0300

    FIX #67
    # Fix Ticket #67: **As a backend developer, I want to fix the document splitting bug**
    
    - Fix Document Splitting Bug
    
    ### Related Ticket: #67

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/utils/documents/StaticWordCountSplitter.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/utils/documents/StaticWordCountSplitter.java
@@ -28,14 +28,21 @@ public class StaticWordCountSplitter implements DocumentSplitter {
         int counter = 0;
         for (String word : document.split(" ")) {
             section.append(word).append(" ");
+            counter++;
 
             // split the section in this word
-            if (++counter == wordCount) {
-                sections.add(section.toString());
+            if (counter == wordCount) {
+                sections.add(section.toString().trim());
                 section = new StringBuilder();
                 counter = 0;
             }
         }
+
+        // Add the remaining section if there is any
+        if (section.length() > 0) {
+            sections.add(section.toString().trim());
+        }
+
         return sections;
     }
 }
