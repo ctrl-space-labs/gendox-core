ab11063fb3aee7058e40231dfa4d1fbc3e3e93a4
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-10-13 17:12:59 +0300

    FIX #75 FIX #76 FIX #77
    # Fix Tickets
     - #75: As a backend developer, I want to implement the chat template option
     - #76: As a backend developer, I want to implement the sectionTemplate option
     - #77: As a backend developer, I want to implement the DocumentSplitter option
    
     ## Τhe following were implemented
    
    - Update chat and section templates
     - ChatTemplate
     - SectionTemplate
     - ServiceName
     - CompletionService
    
    - Update flyway files to run migrate
     - flyway-local.conf
     - flyway-local.conf
     - V20230807_153210__Core_Schema_initial_tables.sql
     - V20230917_123210__Core_Schema_initial_tables.sql
     - V20230929_104913__add_open_ai_completion_models.sql
     - V20231010_173333__Create_templates_Add_columns_at_project_agents_and_agents_types.sql
    
    - Split EmbeddingService to :
     - EmbeddingService
     - TrainingService
     - CompletionService
    
    ### Related Ticket: #75 #76 #77

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/agents/chat/templates/ChatTemplate.java
+++
@@ -1,9 +0,0 @@
-package dev.ctrlspace.gendox.gendoxcoreapi.services.agents.chat.templates;
-
-import dev.ctrlspace.gendox.gendoxcoreapi.model.Message;
-
-
-public interface ChatTemplate {
-    public String chatTemplate(Message message, String sectionValues);
-
-}
