53bad22ecb1c38f1d7010ea34ecbf3f3a411468b
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-11-04 15:04:02 +0200

    WIX 94
    # WIX Ticket
     - #84: As a Backend developer I want to separate document upload from split functionality
    
    **Description**:
    
    - UploadService
      - separate document upload from spli functionality
      - he methods that are for the upload process remain. The rest of the methods will be moved to the new service called splitFileService
    
    - SplitFileService
      - create methods
        - splitDocumentToSections
        - readTxtFileContent
        - downloadFile
        - createSections
        - createSection
    
    - DocumentController
      - create handleFileSplitter method
    
    - DocumentCriteria
      - implement criteria for SplitFileService
    
    - DocumentPredicates
      - implement methods to take documents from criteria
    
    ### Related Ticket: #94

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/specifications/DocumentPredicates.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/specifications/DocumentPredicates.java
@@ -2,36 +2,73 @@ package dev.ctrlspace.gendox.gendoxcoreapi.repositories.specifications;
 
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.util.StringUtils;
+import com.querydsl.jpa.JPAExpressions;
 import dev.ctrlspace.gendox.gendoxcoreapi.model.QDocumentInstance;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.QProjectDocument;
 import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.criteria.DocumentCriteria;
 
 
-
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 public class DocumentPredicates {
 
-    private static QDocumentInstance documentInstance = QDocumentInstance.documentInstance;
+    private static QDocumentInstance qDocumentInstance = QDocumentInstance.documentInstance;
+    private static QProjectDocument qProjectDocument = QProjectDocument.projectDocument;
 
-    public static Predicate build(DocumentCriteria criteria){
+
+    public static Predicate build(DocumentCriteria criteria) {
         return ExpressionUtils.allOf(
-                organizationId(criteria.getOrganizationId())
-               // userId(criteria.getUserId())
+                organizationsId(criteria.getOrganizationId()),
+                projectId(criteria.getProjectId()),
+                documentInstanceId(criteria.getDocumentInstanceId()),
+                documentInstanceIds(criteria.getDocumentInstanceIds())
         );
     }
 
-    private static Predicate organizationId(String organizationId){
-        if (organizationId == null) {
+    private static Predicate organizationsId(String organizationId) {
+        if (StringUtils.isNullOrEmpty(organizationId)) {
             return null;
         }
-        return documentInstance.organizationId.eq(UUID.fromString(organizationId));
+        return qDocumentInstance.organizationId.eq(UUID.fromString(organizationId));
+
     }
-//    private static Predicate userId(String userId) {
-//        if (userId == null) {
-//            return null;
-//        }
-//        return documentInstance.userId.eq(UUID.fromString(userId));
-//    }
+
+    private static Predicate projectId(String projectId) {
+        if (StringUtils.isNullOrEmpty(projectId)) {
+            return null;
+        }
+
+        // Assuming you have a relationship between DocumentInstance and ProjectDocument
+        return qDocumentInstance
+                .id.in(
+                        JPAExpressions
+                                .select(qProjectDocument.documentId)
+                                .from(qProjectDocument)
+                                .where(qProjectDocument.project.id.eq(UUID.fromString(projectId))
+                                ));
+    }
+
+    private static Predicate documentInstanceId(String documentInstanceId) {
+        if (StringUtils.isNullOrEmpty(documentInstanceId)) {
+            return null;
+        }
+        return qDocumentInstance.id.eq(UUID.fromString(documentInstanceId));
+    }
+
+    private static Predicate documentInstanceIds(List<String> documentInstanceIds) {
+        if (documentInstanceIds == null || documentInstanceIds.isEmpty()) {
+            return null;
+        }
+        List<UUID> parsedIds = documentInstanceIds
+                .stream()
+                .map(UUID::fromString)
+                .collect(Collectors.toList());
+
+        return qDocumentInstance.id.in(parsedIds);
+    }
 
 }
--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/UploadService.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/UploadService.java
@@ -1,32 +1,21 @@
 package dev.ctrlspace.gendox.gendoxcoreapi.services;
 
-import dev.ctrlspace.gendox.gendoxcoreapi.converters.DocumentConverter;
+
+
 import dev.ctrlspace.gendox.gendoxcoreapi.exceptions.GendoxException;
 import dev.ctrlspace.gendox.gendoxcoreapi.model.DocumentInstance;
-import dev.ctrlspace.gendox.gendoxcoreapi.model.ProjectAgent;
 import dev.ctrlspace.gendox.gendoxcoreapi.model.ProjectDocument;
-import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.DocumentDTO;
-import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.DocumentInstanceSectionDTO;
-import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.DocumentSectionMetadataDTO;
-import dev.ctrlspace.gendox.gendoxcoreapi.repositories.ProjectAgentRepository;
-import dev.ctrlspace.gendox.gendoxcoreapi.utils.templates.ServiceSelector;
-import dev.ctrlspace.gendox.gendoxcoreapi.utils.templates.documents.DocumentSplitter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ResourceLoader;
-
 import org.springframework.core.io.WritableResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.time.LocalDate;
 
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -38,54 +27,35 @@ public class UploadService {
     private String uploadDir;
 
     private DocumentService documentService;
-    private DocumentConverter documentConverter;
-    private TypeService typeService;
     private ProjectDocumentService projectDocumentService;
-    private ServiceSelector serviceSelector;
-    private ProjectAgentRepository projectAgentRepository;
-
 
     @Autowired
     private ResourceLoader resourceLoader;
 
     @Autowired
     public UploadService(DocumentService documentService,
-                         DocumentConverter documentConverter,
-                         TypeService typeService,
-                         ProjectDocumentService projectDocumentService,
-                         ServiceSelector serviceSelector,
-                         ProjectAgentRepository projectAgentRepository) {
+                         ProjectDocumentService projectDocumentService) {
         this.documentService = documentService;
-        this.documentConverter = documentConverter;
-        this.typeService = typeService;
         this.projectDocumentService = projectDocumentService;
-        this.serviceSelector = serviceSelector;
-        this.projectAgentRepository = projectAgentRepository;
     }
 
 
-    public String uploadFile(MultipartFile file, UUID organizationId, UUID projectId) throws IOException, GendoxException {
+    public DocumentInstance uploadFile(MultipartFile file, UUID organizationId, UUID projectId) throws IOException, GendoxException {
         // Generate a unique file name to avoid conflicts
         UUID documentInstanceId = UUID.randomUUID();
 
-
         String fullFilePath = saveFile(file, organizationId, documentInstanceId);
 
-        // files content
-        String content = readTxtFileContent(file);
+        DocumentInstance instance = new DocumentInstance();
+        instance.setId(documentInstanceId);
+        instance.setOrganizationId(organizationId);
+        instance.setRemoteUrl(fullFilePath);
+        instance = documentService.createDocumentInstance(instance);
 
-        // create DTOs
-        DocumentDTO instanceDTO = createInstanceDTO(documentInstanceId, organizationId, fullFilePath);
-        List<DocumentInstanceSectionDTO> sectionDTOs = createSectionDTOs(instanceDTO, content, projectId);
+        // create project document
+        ProjectDocument projectDocument = projectDocumentService.createProjectDocument(projectId, instance.getId());
 
-        // create document
-        DocumentInstance instance = createFileDocument(instanceDTO, sectionDTOs);
-
-        ProjectDocument projectDocument = new ProjectDocument();
-        projectDocument = projectDocumentService.createProjectDocument(projectId, instance.getId());
-
-
-        return content;
+        return instance;
     }
 
     /**
@@ -103,7 +73,7 @@ public class UploadService {
         String uniqueFileName = documentInstanceId.toString() + "_" + fileName;
 
         String filePathPrefix = calculateFilePathPrefix(organizationId);
-        String fullFilePath = uploadDir + "/" + filePathPrefix + "/" + fileName;
+        String fullFilePath = uploadDir + "/" + filePathPrefix + "/" + uniqueFileName;
 
         createLocalFileDirectory(filePathPrefix);
 
@@ -113,19 +83,6 @@ public class UploadService {
             outputStream.write(bytes);
         }
         return fullFilePath;
-    }
-
-    private String readTxtFileContent(MultipartFile file) throws IOException {
-        StringBuilder fileContent = new StringBuilder();
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                fileContent.append(line).append(" \n ");
-            }
-        }
-
-        return fileContent.toString();
     }
 
 
@@ -139,7 +96,6 @@ public class UploadService {
                 folder.mkdirs(); // Create the folder and it's parent directories if needed
             }
         }
-
 
     }
 
@@ -158,67 +114,6 @@ public class UploadService {
         return folderStructure;
     }
 
-
-    public DocumentInstance createFileDocument(DocumentDTO documentDTO, List<DocumentInstanceSectionDTO> sectionDTO) throws GendoxException {
-        documentDTO.setDocumentInstanceSections(sectionDTO);
-        DocumentInstance documentInstance = documentConverter.toEntity(documentDTO);
-        documentInstance = documentService.createDocumentInstance(documentInstance);
-        return documentInstance;
-
-    }
-
-
-    public DocumentDTO createInstanceDTO(UUID documentInstanceId, UUID organizationId, String location) throws GendoxException {
-        DocumentDTO instanceDTO = new DocumentDTO();
-        instanceDTO.setId(documentInstanceId);
-        instanceDTO.setOrganizationId(organizationId);
-        instanceDTO.setRemoteUrl(location);
-        return instanceDTO;
-    }
-
-
-    public List<DocumentInstanceSectionDTO> createSectionDTOs(DocumentDTO documentDTO, String fileContent, UUID projectId) throws GendoxException{
-        List<DocumentInstanceSectionDTO> sectionDTOS = new ArrayList<>();
-        // take the splitters type
-        ProjectAgent agent = projectAgentRepository.findByProjectId(projectId);
-        String splitterTypeName = agent.getDocumentSplitterType().getName();
-
-        DocumentSplitter documentSplitter = serviceSelector.getDocumentSplitterByName(splitterTypeName);
-        if (documentSplitter == null) {
-            throw new GendoxException("DOCUMENT_SPLITTER_NOT_FOUND", "Document splitter not found with name: " + splitterTypeName, HttpStatus.NOT_FOUND);
-        }
-
-        List<String> contentSections = documentSplitter.split(fileContent);
-
-        Integer sectionOrder = 0;
-        for (String contentSection : contentSections) {
-            sectionOrder++;
-            DocumentInstanceSectionDTO sectionDTO = createSectionDTO(documentDTO, contentSection, sectionOrder);
-            sectionDTOS.add(sectionDTO);
-        }
-
-        return sectionDTOS;
-    }
-
-    public DocumentInstanceSectionDTO createSectionDTO(DocumentDTO documentDTO, String fileContent, Integer sectionOrder) {
-        DocumentInstanceSectionDTO sectionDTO = new DocumentInstanceSectionDTO();
-        DocumentSectionMetadataDTO metadataDTO = createMetadataDTO(sectionOrder);
-        sectionDTO.setDocumentSectionMetadata(metadataDTO);
-        sectionDTO.setSectionValue(fileContent);
-
-        return sectionDTO;
-    }
-
-    public DocumentSectionMetadataDTO createMetadataDTO(Integer sectionOrder) {
-        DocumentSectionMetadataDTO metadataDTO = new DocumentSectionMetadataDTO();
-
-        // Default metadata
-        metadataDTO.setDocumentSectionTypeId(typeService.getDocumentTypeByName("FIELD_TEXT").getId());
-        metadataDTO.setTitle("Default Title");
-        metadataDTO.setSectionOrder(sectionOrder);
-
-        return metadataDTO;
-    }
 }
 
 
