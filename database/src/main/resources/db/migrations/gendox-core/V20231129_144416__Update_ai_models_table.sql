d8ff4a428b1a65cec14f1dba9d00557f9a5232e7
myrto <myrto997@gmail.com>
2023-11-30 14:25:09 +0200

    WIX 111
    #Fix Ticket #111
    -  Updated  aiModel table.
    
    ### Related Ticket: #111

--- database/src/main/resources/db/migrations/gendox-core/V20231129_144416__Update_ai_models_table.sql
+++ database/src/main/resources/db/migrations/gendox-core/V20231129_144416__Update_ai_models_table.sql
@@ -1,30 +1,9 @@
 ALTER TABLE gendox_core.ai_models
-    RENAME COLUMN model_name TO model,
-    RENAME COLUMN type TO name;
+RENAME COLUMN model_name TO model;
 
-ALTER TABLE gendox_core.ai_models
+ALTER TABLE IF EXISTS gendox_core.ai_models
+RENAME COLUMN type TO name;
+
+ALTER TABLE IF EXISTS gendox_core.ai_models
     ADD COLUMN IF NOT EXISTS description TEXT;
 
-UPDATE TABLE IF EXISTS gendox_core.ai_models
-SET model = 'text-embedding-ada-002'
-WHERE url = 'https://api.openai.com/v1/embeddings';
-
-UPDATE TABLE IF EXISTS gendox_core.ai_models
-SET name = 'Ada2'
-WHERE url = 'https://api.openai.com/v1/embeddings';
-
-UPDATE TABLE IF EXISTS gendox_core.ai_models
-SET name = 'Ada2'
-WHERE url = 'https://api.openai.com/v1/embeddings';
-
-UPDATE gendox_core.ai_models
-SET description = 'GPT 3.5 TURBO'
-WHERE model = 'gpt-3.5-turbo';
-
-UPDATE gendox_core.ai_models
-SET description = 'GPT 4'
-WHERE model = 'gpt-4';
-
-UPDATE gendox_core.ai_models
-SET description = 'ADA-002'
-WHERE model = 'text-embedding-ada-002';
