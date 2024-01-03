2ab94069774a670f11056e581842e2be8c34f056
myrto <myrto997@gmail.com>
2024-01-03 15:49:43 +0200

    WIX -Playwright tests -Tests for organization endpoints. Need to add checks for user roles. -Αdded test data with a migration file.

--- gendox-e2e-tests/tests/api-tests/organizations.spec.js
+++ gendox-e2e-tests/tests/api-tests/organizations.spec.js
@@ -23,12 +23,12 @@ test.describe('Organizations CRUD API', () => {
     test('Get Organization by id', async ({ page, request }) => {
 
 
-        const response = await organizations.getOrganizationById(request, token, 'b76b59a3-eea4-476f-9d4b-f178ecf7eb86');
+        const response = await organizations.getOrganizationById(request, token, '41ce6db7-70fd-411d-b3d8-f2d5775ed501');
 
         expect(response.ok()).toBeTruthy();
         let respBody = await response.json();
-        expect(respBody.id).toBe('b76b59a3-eea4-476f-9d4b-f178ecf7eb86');
-        expect(respBody.name).toBe('Testing-Organization');
+        expect(respBody.id).toBe('41ce6db7-70fd-411d-b3d8-f2d5775ed501');
+        expect(respBody.name).toBe('Test User1');
 
 
         await page.pause();
@@ -40,7 +40,7 @@ test.describe('Organizations CRUD API', () => {
 
 
         await page.pause();
-        const response = await organizationUsers.getOrganizationUsersByOrgId(request, token, 'b76b59a3-eea4-476f-9d4b-f178ecf7eb86')
+        const response = await organizationUsers.getOrganizationUsersByOrgId(request, token, '41ce6db7-70fd-411d-b3d8-f2d5775ed501')
 
         expect(response.ok()).toBeTruthy();
         let respBody = await response.json();
@@ -48,12 +48,12 @@ test.describe('Organizations CRUD API', () => {
         expect(respBody.length).toBeGreaterThan(0);
         //iterate all users and get the one that has property role.name === 'ROLE_ADMIN'
         let adminUser = respBody.find(user => user.role.name === 'ROLE_ADMIN');
-        expect(adminUser.user.email).toBe('csekas@test.com');
+        expect(adminUser.user.email).toBe('testuser1@test.com');
 
 
     });
 
-    test('Get All Organization', async ({ page, request }) => {
+    test('Get All Organizations', async ({ page, request }) => {
 
         // https://gendox.ctrlspace.dev/gendox/api/v1/organizations/b76b59a3-eea4-476f-9d4b-f178ecf7eb86/users
         const response = await organizations.getOrganizationByCriteria(request, token, {});
@@ -66,13 +66,13 @@ test.describe('Organizations CRUD API', () => {
 
 
      test('Create Organization', async ({ page, request }) => {
-        const newOrgData = {
+        const newOrganizationData = {
                              name: 'A Testing Organization',
                              displayName: 'Delete Organization',
                              address: 'Middle Of Nowhere'
                          };
 
-        const response = await organizations.createOrganization(request, token, newOrgData);
+        const response = await organizations.createOrganization(request, token, newOrganizationData);
 
         expect(response.ok()).toBeTruthy();
         let respBody = await response.json();
@@ -80,9 +80,43 @@ test.describe('Organizations CRUD API', () => {
         expect(respBody.name).toBe('A Testing Organization');
         expect(respBody.displayName).toBe('Delete Organization');
 
+
         await page.pause();
 
     });
+
+//test if user has writing rights EDITOR_ROLE
+    test.only('Update Organization', async ({ page, request }) => {
+        const updatedOrganizationData = {
+                            id: '19c4a9b1-d9e7-4621-95f4-d006fef265c2',
+                            name: 'A Testing Organization',
+                            displayName: 'Delete this Organization'
+                         };
+
+        const response = await organizations.updateOrganization(request, token, updatedOrganizationData, '19c4a9b1-d9e7-4621-95f4-d006fef265c2');
+
+        expect(response.ok()).toBeTruthy();
+        let respBody = await response.json();
+        expect(respBody.id).toBe('19c4a9b1-d9e7-4621-95f4-d006fef265c2');
+        expect(respBody.name).toBe('A Testing Organization');
+        expect(respBody.displayName).toBe('Delete this Organization');
+
+
+        await page.pause();
+
+        });
+
+//test if user has ADMIN rights ADMIN_ROLE
+
+    test('Delete Organization by id', async ({ page, request }) => {
+        const response = await organizations.deleteOrganization(request, token, '19c4a9b1-d9e7-4621-95f4-d006fef265c2');
+
+        expect(response.ok()).toBeTruthy();
+
+        await page.pause();
+
+
+        });
 
 
 });
