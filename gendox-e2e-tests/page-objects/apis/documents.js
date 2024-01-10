798e692ff3bd398f6a4ae207c04303df2e3f1530
myrto <myrto997@gmail.com>
2024-01-10 16:17:04 +0200

    WIP -get embeddings method - in progress: upload document

--- gendox-e2e-tests/page-objects/apis/documents.js
+++ gendox-e2e-tests/page-objects/apis/documents.js
@@ -1,15 +1,15 @@
 const config = require('../../tests.config');
 
 
-const uploadDocument = async (request, token, projectId, organizationId, filePath) => {
+const uploadDocument = async (request, token, uploadParams) => {
 
-
+// projectId, organizationId, filePath
     const response = await request.post(`${config.gendox.contextPath}/documents/upload`, {
         headers: {
             'Authorization': 'Bearer ' + token,
             'Content-Type': 'application/json'
         },
-        data: { projectId, organizationId, filePath }
+        data: uploadParams
     });
     return response;
 }
---
+++ gendox-e2e-tests/tests/api-tests/documents.spec.js
@@ -0,0 +1,50 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const keycloak = require('../../page-objects/apis/keycloak');
+const documents = require('../../page-objects/apis/documents');
+
+// Tests the Embeddings CRUD API, by performign API calls to the API server.
+// Before all tests, an authentication JWT token is obtained from the API server.
+test.describe('Embeddings CRUD API', () => {
+    let token;
+
+    test.beforeAll(async ({ request }) => {
+
+        //x-www-form-urlencoded
+        const response = await keycloak.simpleUserLogin(request);
+
+        let body = await response.json();
+        token = body.access_token;
+        expect(response.ok()).toBeTruthy();
+    });
+
+//Scenario: Get embeddings for a botRequest.
+//Given that the user is logged in
+//Given the botRequest as an array of strings of messages.
+//When the the AI model is specified .
+//Then the embeddings for the array of messages will be produced.
+
+test('Get Embeddings', async ({ page, request }) => {
+        const uploadParams = {
+                               projectId: ["dress", "hi", "γεια"],
+                               organizationId:
+                               filePath:
+
+                           };
+
+        const response = await documents.upl(request, token, botRequest,{
+                                                        aiModel: 'embed-multilingual-v3.0'});
+
+
+        expect(response.ok()).toBeTruthy();
+        let respBody = await response.json();
+        expect(respBody).toHaveProperty('usage');
+        expect(respBody.data[0]).toHaveProperty('embedding');
+        expect(respBody).toHaveProperty('model');
+        expect(respBody).toHaveProperty('usage');
+
+        await page.pause();
+
+    });
+
+     });
