f25e352299528c56875b585b96f5900c694856fb
myrto <myrto997@gmail.com>
2024-01-15 13:18:11 +0200

    WIP -get embeddings method - in progress: upload document

--- gendox-e2e-tests/tests/api-tests/documents.spec.js
+++ gendox-e2e-tests/tests/api-tests/documents.spec.js
@@ -2,10 +2,11 @@
 const { test, expect } = require('@playwright/test');
 const keycloak = require('../../page-objects/apis/keycloak');
 const documents = require('../../page-objects/apis/documents');
+const path = require('path');
 
-// Tests the Embeddings CRUD API, by performign API calls to the API server.
+// Tests the Embeddings CRUD API, by performing API calls to the API server.
 // Before all tests, an authentication JWT token is obtained from the API server.
-test.describe('Embeddings CRUD API', () => {
+test.describe('Documents CRUD API', () => {
     let token;
 
     test.beforeAll(async ({ request }) => {
@@ -24,24 +25,22 @@ test.describe('Embeddings CRUD API', () => {
 //When the the AI model is specified .
 //Then the embeddings for the array of messages will be produced.
 
-test('Get Embeddings', async ({ page, request }) => {
+test('Upload Document', async ({ page, request }) => {
+
         const uploadParams = {
-                               projectId: ["dress", "hi", "γεια"],
-                               organizationId:
-                               filePath:
+                               projectId: 'dda1148a-2251-4082-b838-6834251f59a0',
+                               organizationId: '41ce6db7-70fd-411d-b3d8-f2d5775ed501',
+//                               file: path.resolve("C:\Users\myrto\OneDrive\Documents\ml.txt")
+                               file: "C:\\Users\\myrto\\OneDrive\\Documents\\ml.txt"
 
                            };
 
-        const response = await documents.upl(request, token, botRequest,{
-                                                        aiModel: 'embed-multilingual-v3.0'});
+        const response = await documents.uploadDocuments(request, token, uploadParams);
 
-
+        console.log(token)
         expect(response.ok()).toBeTruthy();
         let respBody = await response.json();
-        expect(respBody).toHaveProperty('usage');
-        expect(respBody.data[0]).toHaveProperty('embedding');
-        expect(respBody).toHaveProperty('model');
-        expect(respBody).toHaveProperty('usage');
+        expect(respBody.message).toBe("Files uploaded successfully");
 
         await page.pause();
 
