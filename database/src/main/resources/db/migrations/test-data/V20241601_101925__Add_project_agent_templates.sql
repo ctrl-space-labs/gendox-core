dc1e0c3a94b04d98678e46e6d66007ee85e551da
myrto <myrto997@gmail.com>
2024-01-16 11:40:47 +0200

    WIP - Added semantic search and completion. TBD:  - add assertions and description for completion scenario.  - see if different asseritons are need for cohere and openai. - update test data so that project agents include default chat and section templates.

--- database/src/main/resources/db/migrations/test-data/V20241601_101925__Add_project_agent_templates.sql
+++ database/src/main/resources/db/migrations/test-data/V20241601_101925__Add_project_agent_templates.sql
@@ -1,9 +1,8 @@
 UPDATE gendox_core.project_agent
 SET
     chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1),
-    section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
+    section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
     updated_at = NOW()
-
 WHERE
     agent_name = 'test-project-1_1 Agent'
     AND NOT EXISTS (
@@ -11,15 +10,15 @@ WHERE
         FROM gendox_core.project_agent
         WHERE agent_name = 'test-project-1_1 Agent'
           AND chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1)
-          AND section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+          AND section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+    );
 
 
 UPDATE gendox_core.project_agent
 SET
     chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1),
-    section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
+    section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
     updated_at = NOW()
-
 WHERE
     agent_name = 'test-project-1_2 Agent'
     AND NOT EXISTS (
@@ -27,15 +26,15 @@ WHERE
         FROM gendox_core.project_agent
         WHERE agent_name = 'test-project-1_2 Agent'
           AND chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1)
-          AND section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+          AND section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+    );
 
 
 UPDATE gendox_core.project_agent
 SET
     chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1),
-    section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
+    section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
     updated_at = NOW()
-
 WHERE
     agent_name = 'test-project-2_1 Agent'
     AND NOT EXISTS (
@@ -43,14 +42,15 @@ WHERE
         FROM gendox_core.project_agent
         WHERE agent_name = 'test-project-2_1 Agent'
           AND chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1)
-          AND section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+          AND section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+    );
+
 
 UPDATE gendox_core.project_agent
 SET
     chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1),
-    section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
+    section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1),
     updated_at = NOW()
-
 WHERE
     agent_name = 'test-project-2_2 Agent'
     AND NOT EXISTS (
@@ -58,4 +58,5 @@ WHERE
         FROM gendox_core.project_agent
         WHERE agent_name = 'test-project-2_2 Agent'
           AND chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Chat Template' LIMIT 1)
-          AND section_chat_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+          AND section_template_id = (SELECT id FROM gendox_core.templates WHERE name = 'Default Section Template' LIMIT 1)
+    );
