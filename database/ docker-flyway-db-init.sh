fae0aa5b7b638684a41200a5acb86bdbc871c9af
myrto <myrto997@gmail.com>
2024-01-23 10:29:24 +0200

    WIX 165
    Related tickets: 166, 165, 156, 139, 154
    - Implemented logic for moderation  check when moderation is asked for a document

---
+++ database/ docker-flyway-db-init.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+# Set environment variables for PostgreSQL
+export POSTGRES_DB=${POSTGRES_DB:-default_db}
+export POSTGRES_USER=${POSTGRES_USER:-default_user}
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-default_password}
+
+# Set environment variables for Flyway
+export FLYWAY_URL=jdbc:postgresql://localhost:5432/${POSTGRES_DB}
+export FLYWAY_USER=${POSTGRES_USER}
+export FLYWAY_PASSWORD=${POSTGRES_PASSWORD}
+
+echo '----    Restarting DB to enable IPv4 database listener    ----'
+pg_ctl -D "$PGDATA" -m fast -w stop
+pg_ctl -D "$PGDATA" \
+            -o "-c listen_addresses='localhost'" \
+            -w start
+echo '-----------------    Restart complete    ---------------------'
+
+mvn clean install flyway:migrate
