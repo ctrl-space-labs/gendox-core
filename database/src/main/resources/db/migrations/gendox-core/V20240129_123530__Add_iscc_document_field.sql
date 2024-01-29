c20f8e9c4a4c956a404903cc7fae5eba5d7481f7
myrto <myrto997@gmail.com>
2024-01-29 13:01:20 +0200

    WIP 19 - Migrations to add iscc code on document and document sections

---
+++ database/src/main/resources/db/migrations/gendox-core/V20240129_123530__Add_iscc_document_field.sql
@@ -0,0 +1,27 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'document_instance'
+        AND column_name = 'document_iscc_code'
+    ) THEN
+        -- Add the new column
+        ALTER TABLE gendox_core.document_instance
+        ADD COLUMN document_iscc_code VARCHAR(255);
+    END IF;
+END $$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name = 'document_section'
+        AND column_name = 'section_iscc_code'
+    ) THEN
+        -- Add the new column
+        ALTER TABLE gendox_core.document_instance
+        ADD COLUMN section_iscc_code VARCHAR(255);
+    END IF;
+END $$;
