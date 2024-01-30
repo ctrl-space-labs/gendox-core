72d57bb64ea17ce741d0ca0c61bed33e6d29e6ca
myrto <myrto997@gmail.com>
2024-01-30 18:26:09 +0200

    WIP 19 - ISCC CODE upload files with iscc code

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/IsccCodeService.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/IsccCodeService.java
@@ -3,41 +3,56 @@ package dev.ctrlspace.gendox.gendoxcoreapi.services;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+@Service
 public class IsccCodeService {
 
     private static final RestTemplate restTemplate = new RestTemplate();
     String isccCodeApiendpoint = "https://iscc.io/api/v1/iscc";
 
-
     public HttpHeaders buildHeader(String base64EncodedFileName) {
         HttpHeaders headers = new HttpHeaders();
-
         //header for X-Upload-Filename
         headers.add("X-Upload-Filename", base64EncodedFileName);
         return headers;
-
     }
+
     private String encodeFileName(String originalFileName) {
-        // Encode the original file name to base64
         return Base64.getEncoder().encodeToString(originalFileName.getBytes(StandardCharsets.UTF_8));
     }
 
+    public IsccApiResponse getDocumentIsccCode(MultipartFile file, String originalDocumentName) throws IOException {
 
-    public IsccApiResponse getDocumentIsccCode(byte[] fileData, String originalDocumentName) {
+        return this.getDocumentIsccCode(file.getBytes(), originalDocumentName);
+    }
+
+    /**
+     *
+     * @param fileBytes
+     * @param originalDocumentName
+     * @return
+     * @throws IOException
+     */
+    public IsccApiResponse getDocumentIsccCode(byte[] fileBytes, String originalDocumentName) throws IOException {
         String base64EncodedDocumentName = encodeFileName(originalDocumentName);
+
 
         ResponseEntity<IsccApiResponse> responseEntity = restTemplate.postForEntity(
                 isccCodeApiendpoint,
-                new HttpEntity<>(fileData, buildHeader(base64EncodedDocumentName)),
+                new HttpEntity<>(fileBytes, buildHeader(base64EncodedDocumentName)),
                 IsccApiResponse.class);
 
         return responseEntity.getBody();
     }
 
 }
+
