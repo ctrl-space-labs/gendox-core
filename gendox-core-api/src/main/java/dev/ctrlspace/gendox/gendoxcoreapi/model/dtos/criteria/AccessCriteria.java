83b99f5854f716bdb00a733beebb81c4037dab73
myrto <myrto997@gmail.com>
2024-02-20 14:36:19 +0200

    WIP 185

---
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/model/dtos/criteria/AccessCriteria.java
@@ -0,0 +1,16 @@
+package dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.criteria;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class AccessCriteria {
+    private Set<String> orgIds;
+    private Set<String> projectIds;
+}
