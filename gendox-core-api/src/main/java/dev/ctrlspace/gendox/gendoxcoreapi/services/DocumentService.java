92b17c0e2f7264e77a2a4ad230c9b2af94a1a1e7
myrto <myrto997@gmail.com>
2024-02-28 11:45:41 +0200

    WIP 185
    - Added authorization checks on: Documents and Embeddings Controllers.
    - Fixed AiModelConstants
    - Removed mandatory criteria from Get All Projects method.

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/DocumentService.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/DocumentService.java
@@ -5,7 +5,6 @@ import dev.ctrlspace.gendox.gendoxcoreapi.model.*;
 import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.criteria.DocumentCriteria;
 import dev.ctrlspace.gendox.gendoxcoreapi.repositories.*;
 import dev.ctrlspace.gendox.gendoxcoreapi.repositories.specifications.DocumentPredicates;
-import dev.ctrlspace.gendox.gendoxcoreapi.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,8 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -23,6 +20,7 @@ public class DocumentService {
 
     private DocumentInstanceRepository documentInstanceRepository;
     private DocumentSectionService documentSectionService;
+
     private ProjectDocumentService projectDocumentService;
 
     @Autowired
