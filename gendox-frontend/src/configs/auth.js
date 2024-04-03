5ac7ccb7dbcabe203ed61bc16fb9b50be30d9903
Giannis <96373640+gyfantidis@users.noreply.github.com>
2024-04-03 17:31:50 +0300

    FIX ticket #
    **Description**:
     - update url's for dev front-end
    
    ### Related Tickets: #

--- gendox-frontend/src/configs/auth.js
+++ gendox-frontend/src/configs/auth.js
@@ -1,5 +1,5 @@
 export default {
-  getProfile: 'http://localhost:5000/gendox/api/v1/profile',
+  getProfile: 'https://dev.gendox.ctrlspace.dev/gendox/api/v1/profile',
   loginEndpoint: 'https://dev.gendox.ctrlspace.dev/idp/realms/gendox-idp-dev/protocol/openid-connect/token',
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
