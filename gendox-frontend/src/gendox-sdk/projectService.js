b25f016e59ecdbd5bcf536365d008af235169a6b
sekasx <sekas.x@gmail.com>
2024-04-09 11:57:17 +0300

    WIP #215 - Implement Chat App in Frontend
    - Add JS Doc to projectService

--- gendox-frontend/src/gendox-sdk/projectService.js
+++ gendox-frontend/src/gendox-sdk/projectService.js
@@ -2,6 +2,12 @@ import axios from "axios";
 import apiRequests from "../configs/apiRequest";
 
 
+/**
+ * Get all projects by organizationId
+ * @param organizationId
+ * @param storedToken
+ * @returns {Promise<axios.AxiosResponse<ProjectAgent>>}
+ */
 const getProjectsByOrganization = async (organizationId, storedToken) => {
     return axios.get(apiRequests.getProjectsByOrganization(organizationId), {
         headers: {
