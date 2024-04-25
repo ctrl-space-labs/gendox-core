d6532b3a5f9b99c26a827110c88381e1be5b47ed
myrto <myrto997@gmail.com>
2024-04-25 18:34:40 +0300

    WIP 203 - Created Entities repos and sql files for OrganizationDid and WalletKey, In progrress WalletKey service, criteria, predicates.

--- database/src/main/resources/db/migrations/gendox-core/V20240418_144510__Add_public_key.sql
+++ database/src/main/resources/db/migrations/gendox-core/V20240418_144510__Add_public_key.sql
@@ -1,3 +1,4 @@
+-- Check if the column local_key exists before renaming it
 DO $$
 BEGIN
     IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'wallet_keys' AND column_name = 'local_key') THEN
@@ -8,7 +9,7 @@ END $$;
 -- Check if the column jwk_key_format exists before renaming it
 DO $$
 BEGIN
-    IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'wallet_keys' AND column_name = 'jwk_key_format') THEN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'wallet_keys' AND column_name = 'jwk_key_format') THEN
         ALTER TABLE gendox_core.wallet_keys RENAME COLUMN jwk_key_format TO jwk_private_key;
     END IF;
-END $$
+END $$;
