58a882459434b466b6615c29ab2659beeda4b7dd
sekasx <sekas.x@gmail.com>
2024-04-29 17:10:40 +0300

    - Add example update sections API - Add example, update section along with Document instance update - make SecurityUtils#can public and not static

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/DocumentService.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/services/DocumentService.java
@@ -15,12 +15,15 @@ import org.springframework.stereotype.Service;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class DocumentService {
 
     private DocumentInstanceRepository documentInstanceRepository;
+    private DocumentInstanceSectionRepository documentInstanceSectionRepository;
     private DocumentSectionService documentSectionService;
 
     private ProjectDocumentService projectDocumentService;
@@ -28,10 +31,12 @@ public class DocumentService {
     @Autowired
     public DocumentService(DocumentInstanceRepository documentInstanceRepository,
                            DocumentSectionService documentSectionService,
+                           DocumentInstanceSectionRepository documentInstanceSectionRepository,
                            ProjectDocumentService projectDocumentService) {
         this.documentInstanceRepository = documentInstanceRepository;
         this.documentSectionService = documentSectionService;
         this.projectDocumentService = projectDocumentService;
+        this.documentInstanceSectionRepository = documentInstanceSectionRepository;
     }
 
 
@@ -89,6 +94,9 @@ public class DocumentService {
 
         existingDocument.setUpdatedAt(Instant.now());
 
+        updateExistingSectionsList(documentInstance, existingDocument);
+
+        documentInstanceSectionRepository.saveAll(existingDocument.getDocumentInstanceSections());
 
         existingDocument = documentInstanceRepository.save(existingDocument);
 
@@ -96,6 +104,50 @@ public class DocumentService {
     }
 
 
+    /**
+     * Gets the list of sections to be updated, added or deleted from the existing document
+     * Adds new section to the list of existing sections
+     * Updates existing sections
+     * Deletes sections not available in the updated document
+     *
+     * @param updatedDocumentInstance
+     * @param existingDocument
+     */
+    private static void updateExistingSectionsList(DocumentInstance updatedDocumentInstance, DocumentInstance existingDocument) {
+        Set<String> existingSectionIds = existingDocument.getDocumentInstanceSections().stream()
+                .map(DocumentInstanceSection::getId)
+                .map(UUID::toString)
+                .collect(Collectors.toSet());
+        Set<String> newSectionIds = updatedDocumentInstance.getDocumentInstanceSections().stream()
+                .map(DocumentInstanceSection::getId)
+                .map(UUID::toString)
+                .collect(Collectors.toSet());
+
+
+        //Add/update/delete sections from existing document
+        for (DocumentInstanceSection section : updatedDocumentInstance.getDocumentInstanceSections()) {
+            //Handle new sections
+            if (section.getId() == null) {
+                //new section
+                //add metadata to this section
+                existingDocument.getDocumentInstanceSections().add(section);
+            }
+            //hadle update
+            if (existingSectionIds.contains(section.getId().toString())) {
+                //update section
+                // ....
+            }
+        }
+
+        //Delete sections
+        // remove from existing sections the once not available in documentInstance.getDocumentInstanceSections()
+        for (DocumentInstanceSection existingSection : existingDocument.getDocumentInstanceSections()) {
+            if (!newSectionIds.contains(existingSection.getId().toString())) {
+                //delete section
+                existingDocument.getDocumentInstanceSections().remove(existingSection);
+            }
+        }
+    }
 
 
     public void deleteDocument(UUID documentIid, UUID projectId) throws GendoxException {
