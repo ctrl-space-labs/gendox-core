0529bba0c5cbeef41532631e9321117a3636f90f
sekasx <sekas.x@gmail.com>
2024-05-17 21:11:15 +0300

    Update Prod API FQDN

--- gendox-frontend/src/configs/apiRequest.js
+++ gendox-frontend/src/configs/apiRequest.js
@@ -1,5 +1,5 @@
 // const url = "http://localhost:5000/gendox/api/v1/"; // Local Environment
-const url= 'https://gendox.ctrlspace.dev/gendox/api/v1/' // Production Environment (AWS)
+const url= 'https://gendox-api.ctrlspace.dev/gendox/api/v1/' // Production Environment (AWS)
 // const url= 'http://localhost:8080/gendox/api/v1/' // Local Environment
 //const url = 'https://dev.gendox.ctrlspace.dev/gendox/api/v1/' // Development Environment (Hetzner)
 
