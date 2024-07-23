79353d92c286c6573d04e69f225a63f76689fb5e
myrto <myrto997@gmail.com>
2024-07-23 13:32:14 +0300

    WIP 287 - ISCC Config to define iscc beans

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/configuration/ISCCConfig.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/configuration/ISCCConfig.java
@@ -2,20 +2,25 @@ package dev.ctrlspace.gendox.gendoxcoreapi.configuration;
 
 import dev.ctrlspace.provenai.iscc.IsccCodeGeneratorApi;
 import dev.ctrlspace.provenai.iscc.IsccCodeService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ISCCConfig {
 
-    @Bean
-    public IsccCodeService isccCodeService(IsccCodeGeneratorApi isccCodeGeneratorApi) {
-        return new IsccCodeService(isccCodeGeneratorApi);
-    }
+    @Value("${iscc.apis.code-generation}")
+    private String isccCodeGenerationApiUrl;
 
     @Bean
     public IsccCodeGeneratorApi isccCodeGeneratorApi() {
-        return new IsccCodeGeneratorApi();
+        return new IsccCodeGeneratorApi( isccCodeGenerationApiUrl);
+    }
+
+    @Bean
+    public IsccCodeService isccCodeService(IsccCodeGeneratorApi isccCodeGeneratorApi) {
+        return new IsccCodeService(isccCodeGeneratorApi);
     }
 
 }
