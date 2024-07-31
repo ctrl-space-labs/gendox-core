0de0690b80274fd3e7a6314e45c65df7f85cfca7
myrto <myrto997@gmail.com>
2024-07-31 14:49:32 +0300

    WIP 295 - New api: getThreadMessageMetadataByMessageId tol get messages metadata/extra information for a specific message - Returns the entity:MessageMetadataDTOMapping

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/MessageRepository.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/MessageRepository.java
@@ -2,12 +2,22 @@ package dev.ctrlspace.gendox.gendoxcoreapi.repositories;
 
 
 import dev.ctrlspace.gendox.gendoxcoreapi.model.Message;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.MessageMetadataDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, UUID>, QuerydslPredicateExecutor<Message> {
+
+    @Query(name = "MessageMetadataDTO.getMessageMetadataByMessageId", nativeQuery = true)
+    List<MessageMetadataDTO> getMessageMetadataByMessageId(@Param("messageId") UUID messageId);
+
 }
