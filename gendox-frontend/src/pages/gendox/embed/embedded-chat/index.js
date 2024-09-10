e33979e54e7e850d6ce5f7c26c9b7103525660a5
sekasx <sekas.x@gmail.com>
2024-09-10 15:36:15 +0300

    Fix Chat input: - Move embedded chat in a seperate page, without using the theme's standard layout

--- gendox-frontend/src/pages/gendox/embed/embedded-chat/index.js
+++ gendox-frontend/src/pages/gendox/embed/embedded-chat/index.js
@@ -2,17 +2,26 @@
 import React from 'react';
 import AppChat from 'src/pages/gendox/chat';
 import Box from "@mui/material/Box";
-import {useTheme} from "@mui/material/styles";
+import {styled, useTheme} from "@mui/material/styles";
 import {useSettings} from "../../../../@core/hooks/useSettings";
+import BlankLayout from "../../../../@core/layouts/BlankLayout";
+import PoweredByGendox from "../../../../layouts/components/shared-components/PoweredByGendox";
 
 
 // Add any extra configurations here
 const appChatConfig = {
     authProviderOption: 'IFrameAuthProvider',
-    footerContent: 'poweredBy',
+    // footerContent: 'poweredBy', // no need for this, since we are using the PoweredByGendox component directly
     embedView: true,
     chatUrlPath: '/gendox/embed/embedded-chat',
 };
+const StyledWrapper = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh", // Full viewport height
+    width: "100vw", // Full viewport width
+}));
+
 const EmbeddedChatApp = (props) => {
     const theme = useTheme();
 
@@ -20,10 +29,30 @@ const EmbeddedChatApp = (props) => {
     const { skin } = settings;
 
     return (
-        <Box className="embedded-app-chat" sx={{height: "100%"}}>
-            {/* Pass the extra configurations as props to AppChat */}
-            <AppChat {...appChatConfig} />
-        </Box>
+        <StyledWrapper
+            sx={{
+                backgroundImage:
+                    settings.mode === "light"
+                        ? `url('/images/gendox-background-light.webp')`
+                        : `url('/images/gendox-background-dark.webp')`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            }}
+        >
+            <Box className="embedded-app-chat" sx={{flex: 1 }}>
+                {/* Pass the extra configurations as props to AppChat */}
+                <AppChat {...appChatConfig} />
+            </Box>
+            <footer>
+                <Box sx={{
+                    padding: theme.spacing(2),
+                }}>
+                    <PoweredByGendox/>
+                </Box>
+            </footer>
+
+        </StyledWrapper>
     );
 };
 
@@ -31,5 +60,7 @@ const EmbeddedChatApp = (props) => {
 EmbeddedChatApp.authProviderOption = appChatConfig.authProviderOption;
 // // Allow both authenticated and unauthenticated users to access the embedded chat
 EmbeddedChatApp.authGuard = false
+
+EmbeddedChatApp.getLayout = page => <BlankLayout>{page}</BlankLayout>
 
 export default EmbeddedChatApp;
