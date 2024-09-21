0187079d0c86c7b4a3bc06d67e192bb459d77efd
sekasx <sekas.x@gmail.com>
2024-09-22 00:05:42 +0300

    - Use organizationPlanService instead of repository, to handle also the default (free) plans - update createDefaultFreePlan to search by SKU and not the SKU type as multiple plans can bee in the free type - Add temporary the standard models to the free plan

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/utils/constants/SubscriptionTypes.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/utils/constants/SubscriptionTypes.java
@@ -4,6 +4,7 @@ public class SubscriptionTypes {
 
 
     public static final String SKU_TYPE_FREE = "SKU_TYPE_FREE";
+    public static final String SKU_FREE = "gd-free-001";
     public static final String SKU_TYPE_BASIC = "SKU_TYPE_BASIC";
     public static final String SKU_TYPE_PRO = "SKU_TYPE_PRO";
     public static final String SKU_TYPE_BUSINESS = "SKU_TYPE_BUSINESS";
