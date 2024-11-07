d6bb1fc28779b96f89dcd6f908b6d92abe09562c
sekasx <sekas.x@gmail.com>
2024-11-07 12:20:41 +0200

    Fix #334
    - Use Identicon for profile image

--- gendox-frontend/src/utils/identiconUtil.js
+++ gendox-frontend/src/utils/identiconUtil.js
@@ -1,27 +1,32 @@
+import CryptoJS from 'crypto-js';
+
 // Desc: Generated identicon for the provided text
 import Identicon from "identicon.js";
 
+const calculateIdenticonHash = (text) => {
+
+    return CryptoJS.MD5(text).toString(CryptoJS.enc.Hex);
+}
+
+
 export const generateIdenticon = (text) => {
-    if (text) {
-        // Create a hash based on the user’s unique ID or name
-        const hash = text
-            ? text
-                .split("")
-                .reduce((acc, char) => acc + char.charCodeAt(0).toString(16), "")
-            : "defaulthash";
 
-        // Generate the identicon data URI with custom color and transparent background
-        const data = new Identicon(hash, {
-            size: 64,                       // Adjust size if needed
-            margin: 0.2,                    // 20% margin
-            format: "svg",                  // SVG format for better quality
-            foreground: [8, 182, 141, 255], // Custom color #08B68D
-            background: [255, 255, 255, 0]  // Transparent background
-        }).toString();
+    // Append default value to text, even if text is null or undefined
+    const appendedText = (text || "") + "default_hash_1312";
 
-        console.log("Generating Identicon!!!!")
+    const hash = calculateIdenticonHash(appendedText);
 
-        // Construct the src URL for the Avatar component
-        return `data:image/svg+xml;base64,${data}`;
-    }
+
+    // Generate the identicon data URI with custom color and transparent background
+    const data = new Identicon(hash, {
+        size: 64,                       // Adjust size if needed
+        margin: 0.2,                    // 20% margin
+        format: "svg",                  // SVG format for better quality
+        foreground: [8, 182, 141, 255], // Custom color #08B68D
+        background: [8, 182, 141, 30]  // TODO: get color from theme
+    }).toString();
+
+
+    // Construct the src URL for the Avatar component
+    return `data:image/svg+xml;base64,${data}`;
 };
