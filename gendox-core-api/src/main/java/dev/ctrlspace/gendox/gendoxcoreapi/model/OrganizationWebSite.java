2db48292de38aa5002eb5a7d5bd2d9ac400c3298
Giannis Yfantidis <96373640+gyfantidis@users.noreply.github.com>
2024-11-09 14:18:15 +0200

    FIX  - feature #332 OrganizationWebSite   - feature #332 OrganizationWebSite

---
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/model/OrganizationWebSite.java
@@ -0,0 +1,125 @@
+package dev.ctrlspace.gendox.gendoxcoreapi.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "organization_web_sites", schema= "gendox_core" )
+public class OrganizationWebSite {
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @Basic
+    @Column(name = "organization_id", nullable = false)
+    private UUID organizationId;
+    @Basic
+    @Column(name = "name", nullable = false, length = 1024)
+    private String name;
+    @Basic
+    @Column(name = "url", nullable = false, length = 1024)
+    private String url;
+    @Basic
+    @Column(name = "created_at", nullable = true)
+    @CreatedDate
+    private Instant createdAt;
+    @Basic
+    @Column(name = "updated_at", nullable = true)
+    @LastModifiedDate
+    private Instant updatedAt;
+    @Basic
+    @Column(name = "created_by")
+    @CreatedBy
+    private UUID createdBy;
+    @Basic
+    @Column(name = "updated_by")
+    @LastModifiedBy
+    private UUID updatedBy;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(UUID organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UUID createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public UUID getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(UUID updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrganizationWebSite that = (OrganizationWebSite) o;
+        return Objects.equals(id, that.id) && Objects.equals(organizationId, that.organizationId) && Objects.equals(name, that.name) && Objects.equals(url, that.url) && Objects.equals(createdAt, that.createdAt) && Objects.equals(updatedAt, that.updatedAt) && Objects.equals(createdBy, that.createdBy) && Objects.equals(updatedBy, that.updatedBy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, organizationId, name, url, createdAt, updatedAt, createdBy, updatedBy);
+    }
+}
