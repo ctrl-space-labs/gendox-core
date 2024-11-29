0bb309407c113e865705e9c380a9c7872637f686
myrto <myrto997@gmail.com>
2024-11-29 14:04:25 +0200

    #356  - organization profile

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/OrganizationRepository.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/repositories/OrganizationRepository.java
@@ -1,15 +1,24 @@
 package dev.ctrlspace.gendox.gendoxcoreapi.repositories;
 
 import dev.ctrlspace.gendox.gendoxcoreapi.model.Organization;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.authentication.OrganizationProfileDTO;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.authentication.UserOrganizationProjectAgentDTO;
+import jakarta.persistence.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, UUID>, QuerydslPredicateExecutor<Organization> {
 
+
+    @Query(nativeQuery = true, name = "OrganizationProfileDTO.findOrganizationProfileById")
+    OrganizationProfileDTO findRawOrganizationProfileById(@Param("orgId") UUID organizationId, @Param("roleType") String roleType);
 
 }
