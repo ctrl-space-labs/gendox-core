324fb11822b7bf19306ef7a153e629f533b49942
myrto <myrto997@gmail.com>
2024-12-06 12:06:26 +0200

    - Added RateLimit Headers - DurationUtils: Convert time in milliseconds - Adjusted OpenAIAda2respone and Embedding response with the added fields for rate limits and their converters. - Method extractRateLimitHeaders extracts the rate limit header values.

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/model/authentication/OrganizationProfileDTO.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/model/authentication/OrganizationProfileDTO.java
@@ -12,17 +12,14 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 @SqlResultSetMapping(
-        name = "OrganizationProfileDTO",
+        name = "OrganizationProfileMapping",
         classes = @ConstructorResult(
                 targetClass = OrganizationProfileDTO.class,
                 columns = {
                         @ColumnResult(name = "id", type = String.class),
                         @ColumnResult(name = "phone", type = String.class),
                         @ColumnResult(name = "user_type_name", type = String.class),
-                        @ColumnResult(name = "user_type_id", type = Long.class),
-                        @ColumnResult(name = "name", type = String.class),
-                        @ColumnResult(name = "role_id", type = Long.class),
-                        @ColumnResult(name = "role_name", type = String.class)
+                        @ColumnResult(name = "name", type = String.class)
                 }
         )
 )
@@ -32,12 +29,9 @@ import java.time.Instant;
         SELECT
             o.id as id,
             o.phone as phone,
-            :roleType as user_type_name, -- Use roleType dynamically
-            t.id as user_type_id,
+            'ROLE_ADMIN' as user_type_name,
             o.name as name
         FROM gendox_core.organizations o
-        LEFT JOIN gendox_core.types t ON o.user_type_id = t.id
-        LEFT JOIN gendox_core.organization_roles r ON o.id = r.organization_id
         WHERE o.id = :orgId
         """,
         resultSetMapping = "OrganizationProfileMapping"
@@ -48,8 +42,5 @@ public class OrganizationProfileDTO {
     private String id;
     private String phone;
     private String userTypeName;
-    private Long userTypeId;
     private String name;
-    private Long roleId;
-    private String roleName;
 }
