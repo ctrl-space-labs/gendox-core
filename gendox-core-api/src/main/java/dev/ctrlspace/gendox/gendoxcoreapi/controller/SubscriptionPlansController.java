0c2e76284b0d1ffc7298e7e2d4b6bdecf6a12e59
Giannis Yfantidis <96373640+gyfantidis@users.noreply.github.com>
2025-02-05 18:23:17 +0200

    FIX - feature-401-subscription-notification
    - Create controller for subscription-notifications from WooCommerce with organizationId.
    - Create controller for subscription-notifications from WooCommerce with user email.

--- gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/controller/SubscriptionPlansController.java
+++ gendox-core-api/src/main/java/dev/ctrlspace/gendox/gendoxcoreapi/controller/SubscriptionPlansController.java
@@ -1,20 +1,19 @@
 package dev.ctrlspace.gendox.gendoxcoreapi.controller;
 
 import dev.ctrlspace.gendox.gendoxcoreapi.exceptions.GendoxException;
-import dev.ctrlspace.gendox.gendoxcoreapi.model.OrganizationPlan;
-import dev.ctrlspace.gendox.gendoxcoreapi.model.SubscriptionPlan;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.*;
+import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.SubscriptionNotificationDTO;
 import dev.ctrlspace.gendox.gendoxcoreapi.model.dtos.criteria.OrganizationPlanCriteria;
-import dev.ctrlspace.gendox.gendoxcoreapi.services.OrganizationPlanService;
-import dev.ctrlspace.gendox.gendoxcoreapi.services.SubscriptionPlanService;
+import dev.ctrlspace.gendox.gendoxcoreapi.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @RestController
@@ -22,15 +21,25 @@ public class SubscriptionPlansController {
 
     private OrganizationPlanService organizationPlanService;
     private SubscriptionPlanService subscriptionPlanService;
+    private OrganizationService organizationService;
+    private UserOrganizationService userOrganizationService;
+    private UserService userService;
+
 
     @Autowired
     public SubscriptionPlansController(OrganizationPlanService organizationPlanService,
-                                       SubscriptionPlanService subscriptionPlanService) {
+                                       SubscriptionPlanService subscriptionPlanService,
+                                       OrganizationService organizationService,
+                                       UserOrganizationService userOrganizationService,
+                                       UserService userService) {
         this.organizationPlanService = organizationPlanService;
         this.subscriptionPlanService = subscriptionPlanService;
+        this.organizationService = organizationService;
+        this.userOrganizationService = userOrganizationService;
+        this.userService = userService;
     }
 
-//    @PreAuthorize("@securityUtils.hasAuthority('OP_READ_ORGANIZATION_PLAN', 'getRequestedOrgIdFromPathVariable')")
+    //    @PreAuthorize("@securityUtils.hasAuthority('OP_READ_ORGANIZATION_PLAN', 'getRequestedOrgIdFromPathVariable')")
     @GetMapping("/organizations/{organizationId}/subscription-plans")
     public Page<SubscriptionPlan> getAllActivePlans(Pageable pageable) {
         return subscriptionPlanService.getAllActiveSubscriptionPlans(pageable);
@@ -50,9 +59,27 @@ public class SubscriptionPlansController {
         return organizationPlanService.cancelOrganizationPlan(organizationPlanId);
     }
 
-    // APi from woocommerce
+
+    @PreAuthorize("@securityUtils.hasAuthority('OP_READ_ORGANIZATION_PLAN', 'getRequestedOrgIdFromPathVariable')")
+    @PostMapping("/organizations/{organizationId}/subscription-notifications")
+    public OrganizationPlan createOrganizationPlanBySubscriptionNotification(@RequestBody SubscriptionNotificationDTO subscriptionNotificationDTO, @PathVariable UUID organizationId) throws GendoxException {
+        Organization organization = organizationService.getById(organizationId);
+        return organizationPlanService.upsertOrganizationPlan(subscriptionNotificationDTO, organization);
+    }
 
 
+    @PostMapping("/subscription-notifications")
+    public OrganizationPlan createOrganizationPlanBySubscriptionNotification(@RequestBody SubscriptionNotificationDTO subscriptionNotificationDTO) throws GendoxException {
+
+        User user = userService.getByEmail(subscriptionNotificationDTO.getEmail());
+        List<UserOrganization> userOrganizations = userOrganizationService.getUserOrganizationsByUserId(user.getId().toString());
+        // Ensure the user belongs to at least one organization
+        if (userOrganizations == null || userOrganizations.isEmpty()) {
+            throw new GendoxException("NO_ORGANIZATION_FOUND", "User does not belong to any organization", HttpStatus.BAD_REQUEST);
+        }
+        Organization organization = userOrganizations.getFirst().getOrganization();
+        return organizationPlanService.upsertOrganizationPlan(subscriptionNotificationDTO, organization);
+    }
 
 
 }
