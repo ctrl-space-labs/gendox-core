7763036cbfa30c4160a38e4c1f9698d8494abe15
Giannis Yfantidis <96373640+gyfantidis@users.noreply.github.com>
2025-02-14 16:01:40 +0200

    FIX - feature-401-update role's owner permissions
    - update role's owner permissions

--- database/src/main/resources/db/migrations/gendox-core/V20250210_155900__Add_Organization_owner_role.sql
+++ database/src/main/resources/db/migrations/gendox-core/V20250210_155900__Add_Organization_owner_role.sql
@@ -75,3 +75,166 @@ from gendox_core.types r, gendox_core.types p
 where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
   and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_MANAGE_SUBSCRIPTIONS'
   and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_API_KEYS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_EDIT_API_KEYS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_VERIFY_PROVEN_AI_ORG_VP'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_PROVEN_AI_DATAPOD'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_EDIT_PROVEN_AI_DATAPOD'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_DELETE_PROVEN_AI_DATAPOD'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_OFFER_PROVEN_AI_DATAPOD_VC'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_PROVEN_AI_AGENT'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_EDIT_PROVEN_AI_AGENT'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_OFFER_PROVEN_AI_AGENT_VC'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_DELETE_PROVEN_AI_AGENT'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_ORGANIZATION_WEB_SITES'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_EDIT_ORGANIZATION_WEB_SITES'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_DELETE_ORGANIZATION'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id, p.id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_DOCUMENT'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id, p.id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_WRITE_DOCUMENT'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id, p.id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_ADD_USERS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id, p.id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_REMOVE_USERS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id, p.id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_EDIT_PROJECT_SETTINGS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_ORGANIZATION_PLAN'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_EDIT_ORGANIZATION_MODEL_KEYS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
+
+insert into gendox_core.role_permission (role_id, permission_id)
+select r.id as role_id, p.id as permission_id
+from gendox_core.types r, gendox_core.types p
+where r.type_category = 'ORGANIZATION_ROLE_TYPE' and r.name = 'ROLE_OWNER'
+  and p.type_category = 'ORGANIZATION_ROLE_PERMISSION_TYPE' and p.name = 'OP_READ_ORGANIZATION_MODEL_KEYS'
+  and not exists (select * from gendox_core.role_permission where role_id = r.id and permission_id = p.id);
