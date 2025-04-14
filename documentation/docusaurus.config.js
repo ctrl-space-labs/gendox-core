efc4e262ce9fe405810e29c1483962c287c66dd5
sekasx <sekas.x@gmail.com>
2025-04-14 11:53:37 +0300

    Update Gendox documentation
    - minor update to User Manual
    - Make docs/intro.md the home page of the app

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -41,6 +41,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
